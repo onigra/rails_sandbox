d812242e895c77deb88958cce7dff24a23691b49
onigra <3280467rec@gmail.com>
2020-05-04 14:44:56 +0900

    unicornのログをjsonで出す

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,11 @@
+# unicornのログのformatをjsonにする
+# https://github.com/phusion/unicorn/blob/bafc04fe666bc5cb8fb7d0534568041964e1e0c2/FAQ#L31L42
+require 'json'
+
+Configurator::DEFAULTS[:logger].formatter = proc do |severity, timestamp, progname, msg|
+  "#{JSON.dump(service: 'unicorn', severity: severity, timestamp: timestamp, progname: progname, msg: msg)}\n"
+end
+
 rails_root = File.expand_path('../../', __FILE__)
 ENV['BUNDLE_GEMFILE'] = "#{rails_root}/Gemfile"
 
